6ed33515027413c4868acb1e2a012a7d5bb396df
Marcos <Marcos@CBX-POA-MATHEUS>
2015-03-20 17:55:59 -0300

    adição jsf

---
+++ FinancialManagement/src/main/java/br/com/hope/FinalnacialManagement/controller/JSFExample.java
@@ -0,0 +1,14 @@
+package br.com.hope.FinalnacialManagement.controller;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class JSFExample {
+	
+	private String hello;
+	
+	public String getHello() {
+		return hello;
+	}
+
+}
